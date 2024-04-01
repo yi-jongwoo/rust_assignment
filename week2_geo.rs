99810ddb53d98aa8a20139b2562729a715d400fe
yi-jongwoo <82140757+yi-jongwoo@users.noreply.github.com>
2024-04-01 20:09:29 +0900

    Create week2_geo.rs

---
+++ week2_geo.rs
@@ -0,0 +1,46 @@
+// Calculate the magnitude of a vector by summing the squares of its coordinates
+// and taking the square root. Use the `sqrt()` method to calculate the square
+// root, like `v.sqrt()`.
+fn magnitude(vec:&[f64;3]) -> f64 {
+    (vec[0]*vec[0]+vec[1]*vec[1]+vec[2]*vec[2]).sqrt()
+}
+
+// Normalize a vector by calculating its magnitude and dividing all of its
+// coordinates by that magnitude.
+fn normalize(vec:&mut[f64;3]) {
+    let z=magnitude(vec);
+    vec[0]/=z;
+    vec[1]/=z;
+    vec[2]/=z;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn magnitude_normal() {
+        let mut vec = [1.0, 2.0, 9.0];
+        assert!((magnitude(&mut vec) - 9.273618495495704).abs() < f64::EPSILON);
+    }
+
+    #[test]
+    fn normalize_normal() {
+        let mut vec = [1.0, 2.0, 9.0];
+        normalize(&mut vec);
+        assert!((magnitude(&mut vec) - 1.0).abs() < f64::EPSILON);
+    }
+
+    #[test]
+    fn magnitude_unit() {
+        let mut vec = [0.0, 1.0, 0.0];
+        assert!((magnitude(&mut vec) - 1.0).abs() < f64::EPSILON);
+    }
+
+    #[test]
+    fn normalize_unit() {
+        let mut vec = [0.0, 1.0, 0.0];
+        normalize(&mut vec);
+        assert!((magnitude(&mut vec) - 1.0).abs() < f64::EPSILON);
+    }
+}
